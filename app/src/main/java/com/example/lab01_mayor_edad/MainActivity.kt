cccbe6b501c38b126a585360bd55b615cb1fcbbe
Jonathan Reátegui <j.reategui2012@gmail.com>
2021-10-10 02:25:05 -0500

    age validation

--- app/src/main/java/com/example/lab01_mayor_edad/MainActivity.kt
+++ app/src/main/java/com/example/lab01_mayor_edad/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.lab01_mayor_edad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnProcess.setOnClickListener {
+            var age = edtAge.text.toString().toInt()
+
+            if (age >= 18) {
+                tvResult.text = "Usted es mayor de edad"
+            } else {
+                tvResult.text = "Usted es menor de edad"
+            }
+        }
+
     }
 }
